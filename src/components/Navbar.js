40889c620337ea6012e19047d8733f9fb5d84148
dovydasgre <dovydasgrebliunas@gmail.com>
2023-12-12 17:00:46 +0200

    V0.1. Error occurs when trying to buy item.

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <a
+          className="navbar-brand col-sm-3 col-md-2 mr-0"
+          href="http://www.dappuniversity.com/bootcamp"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Marketplace
+        </a>
+        <p style={{ color: 'white', paddingRight: '20px' }}>{this.props.account}</p>
+
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
